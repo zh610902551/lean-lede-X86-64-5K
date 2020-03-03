e4149071242a3d51d1eb77d26987d81da49682d4
zh610902551 <610902551@qq.com>
2020-03-03 15:01:50 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,8 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+- name: 自定义插件
+  run: |
+     cd package
+     git clone  https://github.com/Sweet-Fairy/luci-app-ssr-plus-Jo package/luci-app-ssr-plus-Jo
+     git clone  https://github.com/Leo-Jo-My/luci-app-vssr package/luci-app-ssr-plus-Jo
